f0af898a0ef92f0704c1a342b1bf4d05666926d5
Razzoy <61683221+Razzoy@users.noreply.github.com>
2024-12-20 11:35:21 +0100

    https

--- scripts/socket/socketController.js
+++ scripts/socket/socketController.js
@@ -3,5 +3,5 @@ import { io } from "https://cdn.socket.io/4.3.2/socket.io.esm.min.js";
 // Initializes the socket IO connection
 // Its important to note that during development this should be "localhost", but as soon as
 // we host the backend online on Railway, we need to change this to the correct URL
-export const socket = io("tamagotchibackend-production.up.railway.app");
-export const ioSocket = socket.connect("tamagotchibackend-production.up.railway.app");
+export const socket = io("https://tamagotchibackend-production.up.railway.app");
+export const ioSocket = socket.connect("https://tamagotchibackend-production.up.railway.app");
